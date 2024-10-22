20fc1cd36ec4e2defae719ce152e57a5db38f52b
Juan Ramón Sixto Anello <juan@sixtoanello.com>
2024-10-22 21:35:41 +0200

    fix: add info package version in tests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function helloWorld() {
-    return "Hello, world! Next version 1.0.1";
+    return "Hello, world! Next version 1.0.2";
 }
 
 module.exports = helloWorld;
--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const helloWorld = require('../src/index');
 
 describe('helloWorld', function() {
-  it('should return "Hello, world!"', function() {
-    assert.strictEqual(helloWorld(), 'Hello, world!');
+  it('should return "Hello, world! Next version 1.0.2"', function() {
+    assert.strictEqual(helloWorld(), 'Hello, world! Next version 1.0.2');
   });
 });
